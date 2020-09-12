8c65f4783c39274da5d7b7bbba162acc66042fc1
Lazaro Alvelaez <lazaroalvelaez@bc.edu>
2020-09-12 14:36:08 -0400

    Initial Commit

---
+++ WordGarden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordGarden
+//
+//  Created by Lazaro Alvelaez on 9/12/20.
+//  Copyright © 2020 Lazaro Alvelaez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
